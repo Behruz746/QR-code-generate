6c1467ac78b77025f6f96f987052ad92e929ed5b
Behruz <madaminovbehruz746@gmail.com>
2024-01-23 20:27:23 +0500

    added: new function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { QRCode } from "react-qrcode-logo";
 import "./scss/style.scss";
 
 function App() {
   const [isQrLk, setIsQrLk] = useState(`https://www.bexruz.uz/`);
   const [isQrSz, setIsQrSz] = useState(200);
-  const [isQrLgPath, setIsQrLgPath] = useState("/");
+  const [isQrColor, setIsQrColor] = useState("#ffffff");
+  const [isQrDotColor, setIsQrDotColor] = useState("#000000");
+  const [isQrEyeColor, setIsQrEyeColor] = useState("#000000");
+  const [isQrDot, setIsQrDot] = useState(false);
+  const [isQrLgPath, setIsQrLgPath] = useState(false);
+  const [isQrRadius, setIsQrRadius] = useState(0);
 
+  const removeImage = () => setIsQrLgPath(false);
   const downloadCode = () => {
     const canvas = document.querySelector("#qr-code-id");
     if (canvas) {
@@ -22,31 +28,31 @@ function App() {
     }
   };
 
+  useEffect(() => {
+    console.log(isQrLgPath);
+  }, [isQrLgPath]);
+
   const QRCodeComponent = () => {
     return (
-      <>
+      <div className="qr__container">
         <QRCode
           value={isQrLk} // link
           ecLevel="H" // The error correction level of the QR Code
           size={isQrSz} // size QR code
           logoImage={isQrLgPath} // logo
-          logoWidth={isQrSz * 0.2} // wodth logo
-          logoHeight={isQrSz * 0.2} // height logo
+          logoWidth={isQrSz * 0.4} // wodth logo
+          logoHeight={isQrSz * 0.4} // height logo
           logoOpacity={1} // logo opcity
           removeQrCodeBehindLogo={false} // logo backgroundd
-          eyeColor="#000" // eyes color
-          bgColor="#fff" // background color
-          fgColor="#000" // QR code color
-          eyeRadius={[
-            [10, 10, 0, 10], // top/left eye
-            [10, 10, 10, 0], // top/right eye
-            [10, 0, 10, 10], // bottom/left
-          ]}
+          eyeColor={isQrEyeColor} // eyes color
+          bgColor={isQrColor} // background color
+          fgColor={isQrDotColor} // QR code color
+          eyeRadius={+isQrRadius}
           logoPaddingStyle="square" // style logo square | circle
-          qrStyle="dots" // squares | dots QR dots styles
+          qrStyle={isQrDot ? "dots" : "squares"} // squares | dots QR dots styles
           id="qr-code-id" // QR code elelemt ID
         />
-      </>
+      </div>
     );
   };
 
@@ -76,6 +82,35 @@ function App() {
                 }}
               />
             </div>
+            <div className="color__input">
+              <input
+                type="color"
+                onInput={(e) => setIsQrColor(e.target.value)}
+                value={isQrColor}
+              />
+              <input
+                type="color"
+                onInput={(e) => setIsQrDotColor(e.target.value)}
+                value={isQrDotColor}
+              />
+              <input
+                type="color"
+                onInput={(e) => setIsQrEyeColor(e.target.value)}
+                value={isQrEyeColor}
+              />
+            </div>
+
+            <input
+              type="number"
+              onInput={(e) => setIsQrRadius(e.target.value)}
+              value={isQrRadius}
+            />
+
+            {isQrLgPath && (
+              <button className="qrRemove__btn" onClick={() => removeImage()}>
+                Remove image
+              </button>
+            )}
             <button className="qrCode__btn" onClick={() => downloadCode()}>
               Download QR Code
             </button>
